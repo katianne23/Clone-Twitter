d49ee5bf81155f0983de57c95010814770a3447d
Katianne23 <katianne40@gmail.com>
2023-04-14 11:49:09 -0300

    Twitter

--- src/app/components/avatar/avatar.component.ts
+++ src/app/components/avatar/avatar.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-avatar',
   templateUrl: './avatar.component.html',
-  styleUrls: ['./avatar.component.css']
 })
 export class AvatarComponent {
 
